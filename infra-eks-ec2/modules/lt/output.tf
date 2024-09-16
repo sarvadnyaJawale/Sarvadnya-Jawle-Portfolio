3eaf39dc702b12283cbb6296880c28cf35bd061a
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-09-16 14:34:27 +0530

    Delete infra-eks-ec2/modules directory

--- infra-eks-ec2/modules/lt/output.tf
+++
@@ -1,3 +0,0 @@
-output "eks-ng-lt" {
-    value = aws_launch_template.eks_launch_template.id
-}
--- infra-eks-ec2/modules/sg/output.tf
+++
@@ -1,9 +0,0 @@
-output "alb_security_group" {
-  value = aws_security_group.alb_security_group.id
-}
-output "eks_ng_security_group" {
-  value = aws_security_group.eks_node_security_group.id
-}
-output "ssh_http_sgs" {
-  value = aws_security_group.http_ssh_sg.id
-}
