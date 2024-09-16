3a6fc0b726065d7fe765dfe038e780e5026044a2
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-09-16 14:37:54 +0530

    Add files via upload

---
+++ modules/alb/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_id" {}
+# variable "ec2_public_az1" {}
+# variable "ec2_public_az2" {}
+# variable "ec2_instance_ids" {} 
+variable "alb_security_group" {}
+# variable "public_subnet_ids" {}
+variable "public_subnet_az1" {}
+variable "public_subnet_az2" {}
+variable "public-asg-id" {}
+variable "sg_http_ssh" {}
+# variable "public_subnet_a2" {}
+# variable "acm_certificate_arn" {}
