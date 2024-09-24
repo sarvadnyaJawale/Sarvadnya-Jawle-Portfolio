936e93cce94a32acfb6799b0201dc5ddef4b20b6
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-09-24 11:48:14 +0530

    Update provider.tf

--- infra-eks-ec2/provider.tf
+++ infra-eks-ec2/provider.tf
@@ -3,9 +3,6 @@ provider "aws" {
   region  = var.aws_region                   # The AWS region to deploy resources in  
 
   # Uncomment and set these if using static credentials instead of a profile
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-
   # Optional: Configure a default shared credentials file and default region
   # shared_credentials_file = "/path/to/credentials/file"
   # region = "us-west-2"
